d0f2b2404d086ed3618de07d67eccc8d66c8a083
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-23 13:12:48 +1100

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("message", message => {
+  if (message.author.id === "415330227451985920") message.reply("Want some more **FATASS**");
+});
+
+client.on("message", message => {
+  if (message.author.id === "416405151050366976") message.reply("**Fool** you aren't suppose to be in here.");
+});
+
+client.on("message", message => {
+  if (message.author.id === "415704844221808640") message.reply("**Fool** you aren't suppose to be in here.");
+});
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase()=== "") {
+    msg.reply("");
+  }
+});
